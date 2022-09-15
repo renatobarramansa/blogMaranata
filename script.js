530887aee0509b5a761cdbba916e2cef67c37ead
renatobarramansa <rbvbarra@gmail.com>
2022-09-15 17:45:22 -0300

    Inserção dos cards

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-
-
-
-
+//Efeito de letras digitadas...
 function mostrarFuncao(parametro){  
   const letraSeparada = parametro.innerHTML.split('')
   parametro.innerHTML = ''
@@ -14,3 +11,23 @@ function mostrarFuncao(parametro){
 
 const selecionarP = document.querySelector(".imgTopo p");
 mostrarFuncao(selecionarP)
+
+//Scrool Suave
+
+const seta = document.querySelector('.flex a')
+const caminho = document.getElementById('teste')
+
+
+function scrollToSection(event) {
+  event.preventDefault()
+  const href = event.currentTarget.getAttribute('href')
+  const pegarAtributoDoHref = document.querySelector(href) /** Seleciono o id do elemento que quero pegar */
+  
+  pegarAtributoDoHref.scrollIntoView({
+    behavior:'smooth',
+    block:'start',
+  })
+  
+}
+
+seta.addEventListener('click', scrollToSection)
